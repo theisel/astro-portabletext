0e083f95fe0f6f931e00d5c7011c236fcf3acb64
Tom Theisel <tom.theisel@gmail.com>
2022-07-23 18:13:34 +0100

    test: unknown `type` blocks

--- component/test/components/type.test.js
+++ component/test/components/type.test.js
@@ -20,4 +20,24 @@ type("inline", async () => {
   assert.is($el.text(), "Hello World");
 });
 
+type("unknown.block", async () => {
+  const $ = await fetchContent("type/unknown-block");
+  const $el = $("[data-portabletext-unknown]");
+
+  assert.is($el.length, 1);
+  assert.is($el.get(0).name, "div");
+  assert.is($el.attr("style"), "display:none");
+  assert.is($el.attr("data-portabletext-unknown"), "type");
+});
+
+type("unknown.inline", async () => {
+  const $ = await fetchContent("type/unknown-inline");
+  const $el = $("[data-portabletext-unknown]");
+
+  assert.is($el.length, 1);
+  assert.is($el.get(0).name, "span");
+  assert.is($el.attr("style"), "display:none");
+  assert.is($el.attr("data-portabletext-unknown"), "type");
+});
+
 type.run();

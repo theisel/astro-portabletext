f57de21dc9eef9f208d9133c596e818ea23ad9f2
Tom Theisel <tom.theisel@gmail.com>
2022-07-24 06:31:46 +0100

    style: formatted

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({

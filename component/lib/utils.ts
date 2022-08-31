f328db00ad328039d153605da67a671ab9e13a24
Tom Theisel <tom.theisel@gmail.com>
2022-08-31 16:44:02 +0100

    refactor: utils.ts

--- component/lib/utils.ts
+++ component/lib/utils.ts
@@ -1,12 +1,10 @@
-import type { Node, BlockStyle } from "./types";
+import type { TypedObject, BlockStyle } from "./types";
 
 export { toPlainText } from "@portabletext/toolkit";
 export { mergeComponents } from "./internal";
 
 /**
  * Checks if (node) is of {@link BlockStyle}
- * @param {Node} node
- * @returns {boolean}
  */
-export const isBlockStyle = (node: Node): node is BlockStyle =>
+export const isBlockStyle = (node: TypedObject): node is BlockStyle =>
   typeof (node as BlockStyle).style === "string";

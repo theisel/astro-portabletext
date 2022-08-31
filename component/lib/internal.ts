c32f1700106a6cc9c86108b1cd030e38d9637680
Tom Theisel <tom.theisel@gmail.com>
2022-08-31 16:36:33 +0100

    refactor: internal.ts

--- component/lib/internal.ts
+++ component/lib/internal.ts
@@ -1,8 +1,7 @@
 import type {
-  ComponentOrRecord,
-  PortableTextComponents,
-  SomePortableTextComponents,
   Component,
+  ComponentOrRecord,
+  SomePortableTextComponents,
 } from "./types";
 
 /**
@@ -14,52 +13,49 @@ export function throwError(err: Error | string): never {
 }
 
 /**
- * Checks whether (it) is a component
+ * Returns true if `it` is component
  */
-export function isComponent(it: ComponentOrRecord): it is Component {
+export function isComponent(it: unknown): it is Component {
   return typeof it === "function";
 }
 
 /**
  * Helper to merge {@link SomePortableTextComponents}
  */
-export function mergeComponents(
-  components: SomePortableTextComponents,
-  overrides: SomePortableTextComponents
-): SomePortableTextComponents {
-  return (Object.keys(overrides) as Array<keyof PortableTextComponents>).reduce(
-    (cmps, key) => {
-      const override = overrides[key] as ComponentOrRecord;
+export function mergeComponents<
+  Components extends SomePortableTextComponents,
+  Overrides extends SomePortableTextComponents,
+  MergedComponents = {
+    [Key in keyof (Components & Overrides)]: Key extends keyof (
+      | Overrides
+      | Components
+    )
+      ? Overrides[Key] extends Component
+        ? Overrides[Key]
+        : Components[Key] extends Component
+        ? Overrides[Key]
+        : (Overrides & Components)[Key]
+      : (Overrides & Components)[Key];
+  }
+>(components: Components, overrides: Overrides) {
+  const cmps = { ...components } as unknown as Record<
+    string,
+    ComponentOrRecord
+  >;
 
-      (cmps[key] as ComponentOrRecord) =
-        key in cmps
-          ? merge(cmps[key] as ComponentOrRecord, override)
-          : override;
+  for (const [key, override] of Object.entries(overrides)) {
+    const current = components[key as keyof Components];
 
-      return cmps;
-    },
-    components
-  );
-}
+    const value =
+      !current || isComponent(override) || isComponent(current)
+        ? override
+        : {
+            ...current,
+            ...override,
+          };
 
-/**
- * Helper function used by {@link mergeComponents}
- */
-function merge(
-  parent: ComponentOrRecord,
-  override: ComponentOrRecord
-): ComponentOrRecord {
-  if (isFunction(override) || isFunction(parent)) {
-    return override;
+    (cmps[key] as typeof value) = value;
   }
 
-  return {
-    ...parent,
-    ...override,
-  };
+  return cmps as unknown as MergedComponents;
 }
-
-/**
- * Checks (it) is a function
- */
-const isFunction = (it: unknown) => typeof it === "function";

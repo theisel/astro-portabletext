c60cc0422c0360fd3e7cb2d169ee8137297328e7
Tom Theisel <tom.theisel@gmail.com>
2022-09-25 08:27:03 +0100

    refactor!: deprecate `isBlockStyle`

--- astro-portabletext/src/utils.ts
+++ astro-portabletext/src/utils.ts
@@ -4,6 +4,8 @@ export { toPlainText } from "@portabletext/toolkit";
 export { mergeComponents } from "./internal";
 
 /**
+ * @deprecated Has no purpose anymore
+ *
  * Checks if (node) is of {@link BlockStyle}
  */
 export const isBlockStyle = (node: TypedObject): node is BlockStyle =>

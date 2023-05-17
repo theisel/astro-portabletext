339bd96e9cb852072e2f55716b4c864769d51e0e
Tom Theisel <tom.theisel@gmail.com>
2023-05-18 08:01:38 +1000

    refactor(lib)!: remove `types` export

--- astro-portabletext/lib/components.ts
+++ astro-portabletext/lib/components.ts
@@ -1,11 +1,4 @@
 export { default as Block } from "../components/Block.astro";
-export type { Props as BlockProps } from "../components/Block.astro";
-
 export { default as List } from "../components/List.astro";
-export type { Props as ListProps } from "../components/List.astro";
-
 export { default as ListItem } from "../components/ListItem.astro";
-export type { Props as ListItemProps } from "../components/ListItem.astro";
-
 export { default as Mark } from "../components/Mark.astro";
-export type { Props as MarkProps } from "../components/Mark.astro";

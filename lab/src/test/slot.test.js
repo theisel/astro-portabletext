46efe8b180a1be6980ac187a12fc037234211601
Tom Theisel <tom.theisel@gmail.com>
2024-12-03 14:16:49 +1100

    test: improve slot tests with custom components

--- lab/src/test/slot.test.js
+++ lab/src/test/slot.test.js
@@ -11,9 +11,23 @@ slot("block", async () => {
   assert.is($el.length, 1);
 });
 
+slot("custom block", async () => {
+  const $ = await fetchContent("slot/block-custom");
+  const $el = $("p[data-slot='custom-block']");
+
+  assert.is($el.length, 1);
+});
+
 slot("list", async () => {
   const $ = await fetchContent("slot/list");
   const $el = $("ul[data-slot='list']");
+
+  assert.is($el.length, 1);
+});
+
+slot("custom list", async () => {
+  const $ = await fetchContent("slot/list-custom");
+  const $el = $("ul[data-slot='custom-list']");
 
   assert.is($el.length, 1);
 });
@@ -25,11 +39,27 @@ slot("listitem", async () => {
   assert.is($el.length, 1);
 });
 
+slot("custom listitem", async () => {
+  const $ = await fetchContent("slot/listitem-custom");
+  const $el = $("li[data-slot='custom-listitem']");
+
+  assert.is($el.length, 1);
+  assert.is($el.text(), "List Item 1");
+});
+
 slot("mark", async () => {
   const $ = await fetchContent("slot/mark");
   const $el = $("strong[data-slot='mark']");
 
   assert.is($el.length, 1);
+});
+
+slot("custom mark", async () => {
+  const $ = await fetchContent("slot/mark-custom");
+  const $el = $("strong[data-slot='custom-mark']");
+
+  assert.is($el.length, 1);
+  assert.is($el.text(), "bold");
 });
 
 slot("type", async () => {

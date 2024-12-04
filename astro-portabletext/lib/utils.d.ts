5cbfad96b2cc1ae1f374331bdd0dde7ca2af7de1
Tom Theisel <tom.theisel@gmail.com>
2024-12-04 13:27:05 +1100

    feat(lib): consolidate exports in index.ts
    - Simplify imports by consolidating utils exports in index.ts
    - Deprecate individual utils exports with @deprecated tags.
    - Updated test imports

---
+++ astro-portabletext/lib/utils.d.ts
@@ -0,0 +1,20 @@
+declare module "astro-portabletext/utils" {
+  /**
+   * @deprecated Use `toPlainText` from `astro-portabletext` instead
+   */
+  export function toPlainText(
+    ...args: Parameters<typeof import("@portabletext/toolkit").toPlainText>
+  ): ReturnType<typeof import("@portabletext/toolkit").toPlainText>;
+  /**
+   * @deprecated Use `mergeComponents` from `astro-portabletext` instead
+   */
+  export function mergeComponents(
+    ...args: Parameters<typeof import("./utils").mergeComponents>
+  ): ReturnType<typeof import("./utils").mergeComponents>;
+  /**
+   * @deprecated Use `usePortableText` from `astro-portabletext` instead
+   */
+  export function usePortableText(
+    ...args: Parameters<typeof import("./utils").usePortableText>
+  ): ReturnType<typeof import("./utils").usePortableText>;
+}

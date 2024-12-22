26800442ecb0a9ec1a0880d8ac3b518ec56bb4a1
Tom Theisel <tom.theisel@gmail.com>
2024-12-22 14:10:55 +1100

    feat(demo): update Astro & astro-portabletext, add slot customization (#186)
    Updates dependencies and adds slot-based customization for Portable Text

--- demo/src/components/portabletext/index.ts
+++ demo/src/components/portabletext/index.ts
@@ -1,1 +1,13 @@
-export { default as PortableText } from "./PortableText.astro";
+import Block from "./Block.astro";
+import List from "./List.astro";
+import ListItem from "./ListItem.astro";
+import Mark from "./Mark.astro";
+import Type from "./Type.astro";
+
+export const components = {
+  block: Block,
+  list: List,
+  listItem: ListItem,
+  mark: Mark,
+  type: Type,
+};
